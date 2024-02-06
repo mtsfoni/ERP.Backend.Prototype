8bd39b51557a1661d85b574b499760ab8ddb8db3
MTsfoni <mibau89@gmail.com>
2024-02-06 19:19:12 +0100

    Add Rest-Service
    Signed-off-by: MTsfoni <mibau89@gmail.com>

--- ERP.Backend.Models/Price.cs
+++ ERP.Backend.Models/Price.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ERP.Backend.Models
@@ -12,6 +13,7 @@ namespace ERP.Backend.Models
         public int ArticleId { get; set; }
         public DateTime ValidFrom { get; set; }
         public decimal Amount { get; set; }
+        [JsonIgnore]
         public Article? Article { get; set; }
     }
 }
---
+++ ERP.Backend.REST/Controllers/ArticleController.cs
@@ -0,0 +1,50 @@
+﻿using ERP.Backend.Models;
+using ERP.Backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ERP.Backend.REST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController
+        (IArticleService articleService)
+        : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Article>>> GetArticleList()
+        {
+            return await articleService.GetArticleList();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Article>> CreateArticle([FromBody] Article article)
+        {
+            var id = await articleService.CreateArticle(article);
+            return CreatedAtAction(nameof(GetArticleById), new { id }, article);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Article>> GetArticleById(int id)
+        {
+            var article = await articleService.GetArticleById(id);
+            if (article == null) return NotFound();
+            return article;
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteArticle(int id)
+        {
+            await articleService.DeleteArticle(id);
+            return NoContent();
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdateArticle([FromBody] Article article)
+        {
+            await articleService.UpdateArticle(article);
+            return NoContent();
+        }
+
+    }
+}
