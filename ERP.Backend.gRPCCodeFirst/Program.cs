da9b2be85df9ed9007f41ff21a775a473c266741
MTsfoni <mibau89@gmail.com>
2024-02-18 17:38:45 +0100

    Add docker for all services and add short readme.md

--- ERP.Backend.gRPCCodeFirst/Program.cs
+++ ERP.Backend.gRPCCodeFirst/Program.cs
@@ -3,6 +3,7 @@ using ERP.Backend.gRPCCodeFirst.Services;
 using ERP.Backend.Models;
 using ERP.Backend.Services;
 using Grpc.AspNetCore;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +13,37 @@ builder.Services.AddGrpc();
 builder.Services.AddGrpcReflection();
 
 
-builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    if (!Enum.TryParse(Environment.GetEnvironmentVariable("DATABASE_TYPE"), true, out DatabaseType databaseType))
+    {
+        databaseType = DatabaseType.SQLite;
+        Console.WriteLine("Environment variable DATABASE_TYPE was either not found or has an invalid value");
+        Console.WriteLine("\tFalling back to SQLite");
+    }
+
+    string? connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+    if (string.IsNullOrEmpty(connectionString))
+    {
+        connectionString = builder.Configuration.GetConnectionString("ConnectionString");
+    }
+
+    Console.WriteLine($"DatabaseType is {databaseType}");
+
+    if (databaseType == DatabaseType.PostgreSQL)
+    {
+        options.UseNpgsql(
+            connectionString,
+            x => x.MigrationsAssembly(typeof(ERP.Backend.PostgreSQL.Marker).Assembly.GetName().Name!));
+    }
+    else
+    {
+        options.UseSqlite(
+            connectionString,
+            x => x.MigrationsAssembly(typeof(ERP.Backend.SQLite.Marker).Assembly.GetName().Name!));
+
+    }
+});
 
 builder.Services.AddScoped(typeof(IRepository<>), typeof(EntityFrameworkRepository<>));
 builder.Services.AddScoped(typeof(IArticleService), typeof(ERP.Backend.Services.ArticleService));
---
+++ readme.md
@@ -0,0 +1,9 @@
+### Run easily with docker
+```
+docker-compose up -d
+```
+
+You will find the services under those ports:
+- :5340 - REST (Swagger is active, just visit localhost:5340)
+- :5341 - gRPC
+- :5342 - gRPCCodeFirst (C# only)
